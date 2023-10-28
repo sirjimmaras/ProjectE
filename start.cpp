231d078d85569ace1e826ba6faae0607931a6e6f
sirjimmaras <121199879+sirjimmaras@users.noreply.github.com>
2023-10-28 21:57:42 +0300

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -6,94 +6,90 @@
 const int dimensions = 784;
 const int maximages = 100;
 const int max_capacity = 1000;
-const int k = 1;
+const int k = 3;
+const int m = 100;
 
-float v[dimensions];
+float v[k][dimensions];
+int r[k];
 float t = 3.0;
 int w = 100;
 
 using namespace std;
 
-void createv() {
-    mt19937 gen(random_device{}());
-    normal_distribution<float> distribution(0.0, 1.0);
-    for (int i=0; i<dimensions; i++) v[i] = distribution(gen)+2;
-}
+void vector_from_file(const char* filepath, int vector[], int offset) {
+    ifstream file(filepath,ios::binary);
+    unsigned char byte;
 
-float Euclidean_distance (int* vector1, int*vector2){
-    int sum = 0 ;
-    for (int i=0; i<dimensions; i++)
-        sum = sum + pow( vector1[i] - vector2[i],2 ) ;
-    float distance = sqrt(sum) ;
-    return distance ;
-}
-
-
-// diavazei ena specific vector apo to arxeio kai ton epistrefei, needs delete[] after call
-int* vector_from_file(const string& filepath, int vector_number, int offset) { 
-    if (vector_number < 0) {
-        cerr << "Vector numbering error " << endl;
-        return nullptr;
-    }
-    int* new_vector = new int[dimensions];
-    ifstream binaryFile(filepath, ios::binary);
-    if (!binaryFile.is_open()) {
-        cerr << "Error: Cannot open file " << filepath << endl;
-        return nullptr;
-    }
-    int vectoroffset = offset + vector_number * dimensions;
-    binaryFile.seekg(vectoroffset);
-    if (!binaryFile) {
-        cerr << "Error: Failed to find data" << endl;
-        return nullptr;
-    }
-
-    for (int i = 0; i < dimensions; i++) {
-        unsigned char Value;
-        binaryFile.read((char*)&Value,1) ;
-        new_vector[i] = (int)Value;
-    }
-
-    binaryFile.close();
-    return new_vector;
-}
-
-int exhaustive_search(int images[][dimensions], int* evector) 
-{
-    float min_distance = 100000000.0 ;
-    int vector_number ;
-    float distance ;
-    for (int i=0; i<maximages; i++)
-    {
-        int* currentRow = images[i] ;
-        distance = Euclidean_distance(currentRow, evector) ;
-        if (distance < min_distance)
-            {
-                min_distance = distance ;
-                vector_number = i ;
-            }
-    } 
-    return vector_number ;
-}
-
-
-void hashing(int images[][dimensions], int buckets[][max_capacity], int bucket_size[]) {
-    float product[maximages] = {0.0};
-    int h[maximages];
-    for (int i=0; i<maximages; i++) {
-        for (int j=0; j<dimensions; j++) product[i] += images[i][j]*v[j];
-        h[i] = floor((product[i] + t) / w);
-    }
+    for (int i=0; i<dimensions; i++) {
+        file.seekg(offset, ios::beg);
+        if (file.read((char*)&byte,1)) {
+            // if ((int)byte != 0) cout << (int)byte << " ";
+            vector[i] = (int)byte;            
+        }
+        else cout << "Error" << endl;
+        offset++;
+    }       
 
     cout << endl;
-    cout << "h(p) values:" << endl;
+    file.close();
+}
 
+void create_rand() {
+    mt19937 gen(random_device{}());
+    normal_distribution<float> distribution(0.0, 1.0);
+    for (int kh=0; kh<k; kh++) {
+            for (int i=0; i<dimensions; i++) v[kh][i] = (distribution(gen)+2);
+        if (kh!=0) r[kh] = gen() % 10 + 1;
+        else r[kh] = 1;
+    }
+}
+
+float v_distance (int vector1[], int vector2[], int dim) {
+    int sum = 0 ;
+    for (int i=0; i<dimensions; i++) sum += pow(vector1[i] - vector2[i], dim);
+    float v_distance = pow(sum, 1.0/dim) ;
+    return v_distance;
+}
+
+int exhaustive_search (int query[], int dataset[][dimensions]) {
+    int min_vector = 0;
+    float distance;
+    float min_distance = v_distance(query,dataset[0],2);
     for (int i=0; i<maximages; i++) {
-        cout << h[i] << " " ;
-        int index = h[i] % 10;
+        distance = v_distance(query, dataset[i], 2);
+        if (distance < min_distance) {
+            min_distance = distance;
+            min_vector = i;
+        }
+    }
+    return min_vector;
+}
 
+int hashing(int query[]) {
+    int h;
+    int id = 0;
+    float product = 0.0;
+
+    for (int kh=0; kh<k; kh++) {
+        for (int i=0; i<dimensions; i++) product += query[i]*v[kh][i];
+        h = floor((product + t) / w);
+        id += h*r[kh];
+    }
+    id = id % m;
+    cout << id << " ";
+
+    return id;
+}
+
+void dataset_hash(int images[][dimensions], int buckets[][max_capacity], int bucket_size[]) {
+    create_rand();
+    cout << "id(p) values:" << endl;
+    int id,index;
+    for (int i=0; i<maximages; i++) {
+        id = hashing(images[i]);
+        index = id % 10;
         if(bucket_size[index] < max_capacity){
-            buckets[index][bucket_size[index]] = h[i];
+            buckets[index][bucket_size[index]] = id;
             bucket_size[index]++;
         }
     }
@@ -102,60 +98,37 @@ void hashing(int images[][dimensions], int buckets[][max_capacity], int bucket_s
 
 
 int main() {
-
     const char* file_path = "1.dat";
-    ifstream file(file_path,ios::binary);
-    int numbers[dimensions];
+
+    int query[dimensions];
     int images[maximages][dimensions];
 
-    int byte_index = 16;
-    unsigned char byte;
+    for (int i=0; i<maximages; i++)  {
+        vector_from_file(file_path, images[i], 16+i*dimensions);
+        cout << endl;
+    }
 
-    for (int i=0; i<maximages*dimensions; i++) {
-        file.seekg(byte_index, ios::beg);
-        if (file.read((char*)&byte,1)) {
-            if ((int)byte != 0) cout << (int)byte << " ";
-            if (i % dimensions == 0) cout << endl << endl ;
-            images[i/dimensions][i%dimensions] = (int)byte;
-            byte_index++;
+    int buckets [10][max_capacity] = {{0}};
+    int bucket_size[10] = {0};
+
+
+    dataset_hash(images, buckets, bucket_size);
+
+    for (int i=0; i<10; i++) {
+        cout << "Bucket " << i << ": ";
+        for (int j=0; j<bucket_size[i];j++) {
+            cout << buckets[i][j] << " ";
         }
-        else cout << "End of file reached." << endl;
+        cout << endl << endl ;
     }
 
-    cout << endl;
-    file.close();
-
-int buckets [10][max_capacity] = {{0}};
-int bucket_size[10] = {0};
-
-createv();
-hashing(images, buckets, bucket_size);
-
-for (int i=0; i<10; i++) {
-    cout << "Bucket " << i << ": ";
-    for (int j=0; j<bucket_size[i];j++) {
-        cout << buckets[i][j] << " ";
+    for (int i= 0; i<=110; i++)
+    {
+        vector_from_file("1.dat", query, 16+i*dimensions);
+        int minvec = exhaustive_search(query, images);
+        cout << "The nearest neighbor to vector " << i << " from file is " ;
+        cout << "vector number " << minvec << endl ;
     }
-    cout << endl << endl ;
-}
 
-/*
-for (int query_vector_number = 0; query_vector_number<=666;query_vector_number++)
-{
-    //int query_vector_number = 666 ;
-    int* test = vector_from_file("1.dat", query_vector_number, 16) ;
-    int k = exhaustive_search(images, test);
-    cout << "The nearest neighbor to vector " << query_vector_number << " from file is " ;
-    cout << "vector number " << k << endl ;
-}*/
-
-/*
-int x = 5 ;
-int* test = vector_from_file("1.dat", x, 16) ;
-cout << "Printing vector " << x << endl ;
-for (int i = 0; i < dimensions; i++) 
-    cout << test[i] << " " ;
-cout << endl ;
-delete[] test ;
-return 0;*/
+    return 0;
 }
