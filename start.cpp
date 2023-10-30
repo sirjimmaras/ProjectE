27aa67a7c71679d3489de8ac965d3b17d3ceacf0
sirjimmaras <121199879+sirjimmaras@users.noreply.github.com>
2023-10-30 08:21:12 +0200

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -4,15 +4,18 @@
 #include <cmath>
 
 const int dimensions = 784;
-const int maximages = 100;
+const int maximages = 10;
 const int max_capacity = 1000;
 const int k = 3;
 const int m = 100;
+const int max_hc = 1000*k;
 
 float v[k][dimensions];
 int r[k];
 float t = 3.0;
 int w = 100;
+int hcmap[max_hc];
+int cluster[k][dimensions];
 
 using namespace std;
 
@@ -22,26 +25,11 @@ void vector_from_file(const char* filepath, int vector[], int offset) {
 
     for (int i=0; i<dimensions; i++) {
         file.seekg(offset, ios::beg);
-        if (file.read((char*)&byte,1)) {
-            // if ((int)byte != 0) cout << (int)byte << " ";
-            vector[i] = (int)byte;            
-        }
+        if (file.read((char*)&byte,1))  vector[i] = (int)byte;            
         else cout << "Error" << endl;
         offset++;
     }       
-
-    cout << endl;
     file.close();
-}
-
-void create_rand() {
-    mt19937 gen(random_device{}());
-    normal_distribution<float> distribution(0.0, 1.0);
-    for (int kh=0; kh<k; kh++) {
-            for (int i=0; i<dimensions; i++) v[kh][i] = (distribution(gen)+2);
-        if (kh!=0) r[kh] = gen() % 10 + 1;
-        else r[kh] = 1;
-    }
 }
 
 float v_distance (int vector1[], int vector2[], int dim) {
@@ -51,10 +39,95 @@ float v_distance (int vector1[], int vector2[], int dim) {
     return v_distance;
 }
 
+void clustering(int images[][dimensions], int cluster[][dimensions], int centroid[][max_capacity], int centroid_size[]) {
+    int index;
+    float distance;
+    float min_distance = 1000000.0;
+    // static int step = 1;
+    // if (step == 4) return;
+    for (int i=0; i<maximages; i++) {
+        for (int kh=0; kh<k; kh++) {
+            distance = v_distance(images[i],cluster[kh],2);
+            if (distance < min_distance) {
+                min_distance = distance;
+                index = kh;
+            }
+        }
+        if(centroid_size[index] < max_capacity) {
+            centroid[index][centroid_size[index]] = i;
+            centroid_size[index]++;
+        }
+    }
+    int new_centroid[k][dimensions];
+    int sum=0;
+    for (int kh=0; kh<k; kh++) {
+        for (int i=0; i<dimensions; i++) {
+            for (int j=0; j<centroid_size[kh]; j++) sum += images[centroid[kh][j]][i];
+            if (centroid_size[kh]!=0) new_centroid[kh][i] = sum / centroid_size[kh];
+            else new_centroid[kh][i] = 0;
+            // step++;
+        }
+    }
+    // for (int kh=0;kh<k; kh++) centroid [k][max_capacity] = {0};
+    // centroid_size[k] = {0};
+    // clustering(images,new_centroid,centroid,centroid_size);
+}
+
+int silhouette(int images[][dimensions],int cluster[][dimensions], int centroid[][max_capacity], int centroid_size[]) {
+    int closest;
+    float avg_dist,sum,avg_dist_b,sum_b,dist,min_dist,s;
+    min_dist = 1000000.0;
+    dist = min_dist - 1.0;
+    
+    for (int kh=0; kh<k; kh++) {
+        for (int c=0; c<k; c++) {
+            if (c!=kh) dist = v_distance(images[centroid[kh][0]],cluster[c],2);
+            if(dist<min_dist) min_dist = dist;
+            closest = c;
+        }
+        for (int i=0; i<centroid_size[kh]; i++) {
+            sum += v_distance(images[centroid[kh][0]],images[centroid[kh][i]],2);
+        }
+        avg_dist = sum/centroid_size[kh];
+        for (int i=0; i<centroid_size[kh]; i++) {
+            sum_b += v_distance(images[centroid[closest][0]],images[centroid[closest][i]],2);
+        }
+        avg_dist_b = sum/centroid_size[kh];        
+    }
+    if (avg_dist>avg_dist_b) s = (avg_dist_b-avg_dist)/avg_dist;
+    else s = (avg_dist_b-avg_dist)/avg_dist_b;
+    return s;
+}
+
+void create_rand() {
+    mt19937 gen(random_device{}());
+    normal_distribution<float> distribution(0.0, 1.0);
+    for (int kh=0; kh<k; kh++) {
+            for (int i=0; i<dimensions; i++) {
+                v[kh][i] = (distribution(gen)+2);
+                cluster[kh][i] = gen()%256;
+            }
+        if (kh!=0) r[kh] = gen() % 10 + 1;
+        else r[kh] = 1;
+    }
+    for (int i=0; i<max_hc; i++) hcmap[i] = gen() % 2;
+}
+
+int hamm_distance(int x, int y) {
+    int distance = 0;
+    for (int i = 0; i < sizeof(int) * 8; i++) {
+        int bit1 = (x >> i) & 1;
+        int bit2 = (y >> i) & 1;
+
+        if (bit1 != bit2) distance++;
+    }
+    return distance;
+} 
+
 int exhaustive_search (int query[], int dataset[][dimensions]) {
     int min_vector = 0;
     float distance;
-    float min_distance = v_distance(query,dataset[0],2);
+    float min_distance = 1000000.0;
     for (int i=0; i<maximages; i++) {
         distance = v_distance(query, dataset[i], 2);
         if (distance < min_distance) {
@@ -76,59 +149,97 @@ int hashing(int query[]) {
         id += h*r[kh];
     }
     id = id % m;
-    cout << id << " ";
-
     return id;
 }
 
-void dataset_hash(int images[][dimensions], int buckets[][max_capacity], int bucket_size[]) {
-    create_rand();
-    cout << "id(p) values:" << endl;
+int hypercube(int query[]) {
+    int h;
+    int index = 0;
+    float product = 0.0;
+    for (int kh=0; kh<k; kh++) {
+        for (int i=0; i<dimensions; i++) product += query[i]*v[kh][i];
+        h = floor((product + t) / w);
+        index += hcmap[h % max_hc] * pow(2,kh);
+    }
+    return index;
+}
+
+void dataset_hc_hash(int images[][dimensions], int hc_buckets[][max_capacity],int hc_bucket_size[]) {
+    int index;
+    for (int i=0; i<maximages; i++) {
+        index = hypercube(images[i]);
+        if(hc_bucket_size[index] < max_capacity){
+            hc_buckets[index][hc_bucket_size[index]] = i;
+            hc_bucket_size[index]++;
+        }
+    }
+}
+
+void dataset_hash(int images[][dimensions], int buckets[][max_capacity], int bucket_size[], int imginfo[][2]) {
     int id,index;
     for (int i=0; i<maximages; i++) {
         id = hashing(images[i]);
+        imginfo[i][0] = id;
         index = id % 10;
         if(bucket_size[index] < max_capacity){
-            buckets[index][bucket_size[index]] = id;
+            buckets[index][bucket_size[index]] = i;
             bucket_size[index]++;
+            imginfo[i][1] = index;
         }
     }
-    cout << endl << endl;
 }
 
+int lsh_search (int query[], int buckets[][max_capacity], int bucket_size[], int imginfo[][2], int dataset[][dimensions]) {
+    int min_vector = -1;
+    int position;
+    float distance;
+    float min_distance = 1000000.0;
+    int id = hashing(query);
+    int index = id % 10;
+    for (int i=0; i<bucket_size[index]; i++) {
+        position = buckets[index][i];
+        if (imginfo[position][0] == id) {
+            distance = v_distance(query, dataset[position],2);
+            if (distance < min_distance) {
+                min_distance = distance;
+                min_vector = position;
+            }
+        }
+    }
+    return min_vector;
+}
 
 int main() {
-    const char* file_path = "1.dat";
+    create_rand();
+    const char* file_path = "input.dat";
 
     int query[dimensions];
     int images[maximages][dimensions];
+    int s = 0;
+    int es = 0;
 
     for (int i=0; i<maximages; i++)  {
         vector_from_file(file_path, images[i], 16+i*dimensions);
-        cout << endl;
     }
 
     int buckets [10][max_capacity] = {{0}};
     int bucket_size[10] = {0};
+    int imginfo[maximages][2];
+    int hc_bnum = pow(2,k);
+    int hc_buckets [hc_bnum][max_capacity] = {{0}};
+    int hc_bucket_size[hc_bnum] = {0};
+    int centroid [k][max_capacity] = {{0}};
+    int centroid_size [k] = {0};
 
 
-    dataset_hash(images, buckets, bucket_size);
-
-    for (int i=0; i<10; i++) {
-        cout << "Bucket " << i << ": ";
-        for (int j=0; j<bucket_size[i];j++) {
-            cout << buckets[i][j] << " ";
-        }
-        cout << endl << endl ;
-    }
-
-    for (int i= 0; i<=110; i++)
-    {
-        vector_from_file("1.dat", query, 16+i*dimensions);
-        int minvec = exhaustive_search(query, images);
-        cout << "The nearest neighbor to vector " << i << " from file is " ;
-        cout << "vector number " << minvec << endl ;
-    }
+    dataset_hash(images, buckets, bucket_size, imginfo);
+    dataset_hc_hash(images, hc_buckets, hc_bucket_size);
+    // lsh_search(images[0],buckets,bucket_size,imginfo,images);
+    // hypercube(images[0]);
+    es = exhaustive_search(images[3], images);
+    cout <<"NN to provided query is image no"<< es << endl;
+    clustering (images,cluster,centroid,centroid_size);
+    s = silhouette(images,cluster,centroid,centroid_size);
 
     return 0;
 }
